7dbc846778cff1c509bdde2f927803fc137e158a
duanshengliu <44742794+duanshengliu@users.noreply.github.com>
2020-07-07 13:50:35 +0800

    UI界面，主程序入口
    UI界面，主程序入口

--- License-plate-recognition/UI.py
+++ License-plate-recognition/UI.py
@@ -59,7 +59,7 @@ class Window:
         self.img_src_path = Entry(self.win, state='readonly', text=sv).get()  # 获取到所打开的图片
         img_open = Image.open(self.img_src_path)
         if img_open.size[0] * img_open.size[1] > 240 * 80:
-            img_open=img_open.resize((512,512))
+            img_open=img_open.resize((512,512), Image.ANTIALIAS)
         self.img_Tk = ImageTk.PhotoImage(img_open)
         self.can_src.create_image(258, 258, image=self.img_Tk, anchor='center')
 
@@ -70,7 +70,7 @@ class Window:
             img_src = cv2.imdecode(np.fromfile(self.img_src_path, dtype=np.uint8), -1)  # 从中文路径读取时用
             h, w = img_src.shape[0], img_src.shape[1]
             if h * w <= 240 * 80 and 2 <= w / h <= 5:  # 满足该条件说明可能整个图片就是一张车牌,无需定位,直接识别即可
-                lic = cv2.resize(img_src, dsize=(240, 80))[:, :, :3]  # 直接resize为(240,80)
+                lic = cv2.resize(img_src, dsize=(240, 80), interpolation=cv2.INTER_AREA)[:, :, :3]  # 直接resize为(240,80)
                 img_src_copy, Lic_img = img_src, [lic]
             else:  # 否则就需通过unet对img_src原图预测,得到img_mask,实现车牌定位,然后进行识别
                 img_src, img_mask = unet_predict(self.unet, self.img_src_path)
